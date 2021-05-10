219050c1855d6be466098a10fa4150e734eaacfc
PrathameshDevshatwar12345 <prathamesh.devshatwar@gmail.com>
2021-05-10 16:47:17 +0530

    New File Added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -28,13 +29,16 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log2 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
+    log3 = new Log(760,120,150, PI/7);
+    log4 = new Log(870,120,150, -PI/7);
+    
+    
+    Log5 = new Log(100,70,80, PI/2);
     bird = new Bird(100,100);
+    chain = new Chain(Log5.body,bird.body);
 
 }
 
@@ -53,12 +57,15 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
-    log3.display();
+    log2.display();
 
     box5.display();
+    log3.display();
     log4.display();
-    log5.display();
 
+    Log5.display();
     bird.display();
+    chain.display();
+    
     platform.display();
 }
